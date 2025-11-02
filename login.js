b3e150967ca99f44de9d99716bedae0aa2caab4d
nobita <jescriba.0126@umak.edu.ph.>
2025-11-02 15:57:38 +0800

    kupal

---
+++ login.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.container');
+const registerBtn = document.querySelector('.register-btn');
+const loginBtn = document.querySelector('.login-btn');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+});
+
+const form = document.getElementById('loginForm');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // para hindi mag-refresh ang page
+
+  // kunin ang input values
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // example login check (pwede mong palitan kung gusto mo)
+  if (username === "admin" && password === "1234") {
+    // kapag tama ang username at password, pupunta sa ibang HTML file
+    window.open("main.html", "_self");
+  } else {
+    alert("Invalid username or password!");
+  }
+});
